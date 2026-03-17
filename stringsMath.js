6b8404d08f4f855f58f234cb360cc26c4a534fc1
Akash.T <akashchicku4@gmail.com>
2026-03-17 23:43:27 +0530

    Leaned string and number in js

--- stringsMath.js
+++ stringsMath.js
@@ -9,3 +9,8 @@ console.log(`Total before tax: $${(5788 + 998) / 100}`)
 
 // Template `` string (Interpolation)
 console.log(`Estimated tax (10%): $${Math.round(6786 * 0.1) / 100}`)
+
+// Template `` string (Interpolation)
+console.log(
+  `Order Total: $${(((2095 * 2) + (799 * 2)) / 100) + ((499 + 499) / 100) + (Math.round(6786 * 0.1) / 100)}`
+);
